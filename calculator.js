f59b2230322e46d76a1ceecce67451599437f3d4
Nataliy_Anohina2023 <117434665+Natalia2023@users.noreply.github.com>
2023-04-30 15:49:37 +0300

    ///

--- calculator.js
+++ calculator.js
@@ -1,21 +1,47 @@
-let input = document.getElementById('result');
+// Отримання елементів DOM
+const result = document.getElementById('result');
+const buttons = document.querySelectorAll('.button');
+const operators = document.querySelectorAll('.operator');
+const clearButton = document.querySelector('.clear');
+const equalButton = document.querySelector('.equal');
 
+// Функція, яка додає цифри до введеного значення
 function addNumber(number) {
-  input.value += number;
+  result.value += number;
 }
 
+// Функція, яка додає оператори до введеного значення
 function addOperator(operator) {
-  input.value += operator;
+  result.value += operator;
 }
 
+// Функція, яка очищує введене значення
 function clearResult() {
-  input.value = '';
+  result.value = '';
 }
 
+// Функція, яка рахує результат
 function calculate() {
-  try {
-    input.value = eval(input.value);
-  } catch (e) {
-    input.value = 'Error';
-  }
+  result.value = eval(result.value);
 }
+
+// Надання обробника подій для кожної кнопки
+buttons.forEach(function(button) {
+  button.addEventListener('click', function() {
+    addNumber(this.textContent);
+  });
+});
+
+operators.forEach(function(operator) {
+  operator.addEventListener('click', function() {
+    addOperator(this.textContent);
+  });
+});
+
+clearButton.addEventListener('click', function() {
+  clearResult();
+});
+
+equalButton.addEventListener('click', function() {
+  calculate();
+});
